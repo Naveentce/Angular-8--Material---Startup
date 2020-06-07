5e37480fc29cecea39f17f6cd294adecfe142ed0
Naveen Chandran Selvaraj <naveenchandran.selvaraj@idc.innovasolutions.int>
2020-06-07 23:51:29 +0530

    JQWidgets and Gallery Scroll View Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
 
+
+/** Angular Material Imports */
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav'
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { AppRoutingModule } from './app-routing.module';
+
+/** Application Components Imports */
 import { AppComponent } from './app.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { GalleryComponent } from './gallery/gallery.component';
 import { ActivitiesComponent } from './activities/activities.component';
 import { ServicesComponent } from './services/services.component';
 import { CourseDetailsComponent } from './course-details/course-details.component';
+
+/** Angular JQWidgets Imports */
+import { jqxScrollViewModule } from 'jqwidgets-ng/jqxscrollview';
+import { jqxButtonModule } from 'jqwidgets-ng/jqxbuttons';
 
 @NgModule({
   declarations: [
@@ -27,13 +35,15 @@ import { CourseDetailsComponent } from './course-details/course-details.componen
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
     MatToolbarModule,
     MatSidenavModule,
     MatListModule,
     MatButtonModule,
-    MatIconModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
+    MatIconModule,   
+    jqxScrollViewModule, 
+    jqxButtonModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,11 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild, ViewEncapsulation,OnInit } from '@angular/core';
+import { jqxScrollViewComponent } from 'jqwidgets-ng/jqxscrollview';
 
 @Component({
   selector: 'app-gallery',
   templateUrl: './gallery.component.html',
-  styleUrls: ['./gallery.component.scss']
+  styleUrls: ['./gallery.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class GalleryComponent implements OnInit {
+
+  @ViewChild('myScrollView', { static: false }) myScrollView: jqxScrollViewComponent;
+    onStartClicked(): void {
+        this.myScrollView.slideShow(true);
+    };
+    onStopClicked(): void {
+        this.myScrollView.slideShow(false);
+    };
 
   constructor() { }
 
